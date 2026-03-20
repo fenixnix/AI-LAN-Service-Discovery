aae29eb0e448a4cdbbb3f05183790bff23393903
Nix_Liu <57319138@qq.com>
2026-03-20 21:59:50 +0800

    Update release.ps1

--- utils/release.ps1
+++ utils/release.ps1
@@ -1,4 +1,4 @@
-git tag -d v0.1.4
-git push origin --delete v0.1.4
-git tag -a v0.1.4 -m "Release version 0.1.4"
-git push origin v0.1.4
+git tag -d v0.1.5
+git push origin --delete v0.1.5
+git tag -a v0.1.5 -m "Release version 0.1.5"
+git push origin v0.1.5
